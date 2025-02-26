d950e606813fdaa7ec07ed135ed414584fcab9e2
JitBanerjee03 <Jitbanerjee294@gmail.com>
2025-02-26 15:09:01 +0530

    Deploying the website in a deployment platform

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const app=express();
 const db=require('./db');
 
 app.use(cookieParser())
-app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+app.use(cors({credentials:true,origin:'http://localhost:5174'}));
 const bodyParser=require('body-parser');
 
 // parse application/x-www-form-urlencoded
